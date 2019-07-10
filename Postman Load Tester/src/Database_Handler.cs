d46e42eaab3bb357a26cacec8a7c39df13612a21
Brandon Addison <brandon@causalnexus.com>
2019-07-11 01:00:58 +0200

    Work in Progress - Added Database_Handler - Moved all code to /src folder

---
+++ Postman Load Tester/src/Database_Handler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postman_Load_Tester
+{
+    public class Database_Handler
+    {
+        public void Connect()
+        {
+            //TODO
+        }
+
+        public void AddEnvironment()
+        {
+            //TODO
+        }
+
+        public void AddCollection()
+        {
+            //TODO
+        }
+
+        public void AddApiKey()
+        {
+            //TODO
+        }
+    }
+}
